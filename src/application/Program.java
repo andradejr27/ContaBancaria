7ae7637f1bf9d5a04da604468c3dd6ad1feca3ca
Joao Andrade Junior <junior.bsdlages@hotmail.com>
2020-02-12 13:22:03 -0400

    Adicionado try/catch para tratamento de erros

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import model.entities.Account;
 
@@ -8,25 +9,30 @@ public class Program {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter account data:");
-		System.out.print("Number: ");
-		int number = sc.nextInt();
-		System.out.print("Holder: ");
-		sc.nextLine();
-		String holder = sc.nextLine();
-		System.out.print("Initial balance: ");
-		double balance = sc.nextDouble();
-		System.out.print("Withdraw limit: ");
-		double withdrawLimit = sc.nextDouble();
-
-		Account acc = new Account(number, holder, balance, withdrawLimit);
-
-		System.out.println();
-		System.out.print("Enter amount for withdraw: ");
-		double amount = sc.nextDouble();
-		acc.withdraw(amount);
-
+		
+		try {
+			System.out.println("Enter account data:");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+	
+			Account acc = new Account(number, holder, balance, withdrawLimit);
+	
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			double amount = sc.nextDouble();
+			acc.withdraw(amount);
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Erro! Tipo de dado digitado inválido!");
+		}
+		
 		sc.close();
 
 	}
